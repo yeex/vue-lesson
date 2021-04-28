4bc686a0c9548f50aea0399ce25f61c1f4957c83
Ntwari Moise <mozayntwali@gmail.com>
2021-04-28 22:36:13 +0200

    Hot module replacement

---
+++ src/components/Counter.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h2>The count is : {{ count }}</h2>
+
+        <p>
+            <button @click="count += 10">
+                Increment
+            </button>
+        </p>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return { count : 0 };
+        }
+    }
+</script>
